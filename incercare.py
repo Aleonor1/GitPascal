daf020bde9f1bdd5d8f3200fb66bd365f8372fe5
Aleonor1 <adrianaleodor@gmail.com>
2020-06-22 21:08:19 +0300

    comment from python script

--- incercare.py
+++ incercare.py
@@ -5,13 +5,10 @@ PATH_OF_GIT_REPO = r'/Users/adriannyikita/Desktop/GitPascal/.git'  # make sure .
 COMMIT_MESSAGE = 'comment from python script'
 
 def git_push():
-    try:
         repo = Repo(PATH_OF_GIT_REPO)
         repo.git.add(update=True)
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
         origin.push()
-    except:
-        print('Some error occured while pushing the code')
 
 git_push()
